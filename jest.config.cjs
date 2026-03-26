7e5983a385f5dde6d56497724fecb7edd216ae7b
Joseph <jcm6537@psu.edu>
2026-03-26 15:11:30 -0400

    my Test so far

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,9 +4,12 @@ const tsJestPreset = createDefaultPreset({
   tsconfig: "tsconfig.jest.json",
 });
 
+/** @type {import("jest").Config} */
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
     ...tsJestPreset.transform,
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  testMatch: ["**/?(*.)+(test).ts"],
 };
